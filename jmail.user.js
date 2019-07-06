0cee9f3745a16db6fd6a8429244608b75b450597
Kacper <kacper@cvgo.re>
2019-07-06 15:50:39 +0200

    Create jmail.user.js

---
+++ jmail.user.js
@@ -0,0 +1,63 @@
+(function() {
+    'use strict';
+
+    function fmtMail(value) {
+    	value = typeof value === 'undefined' ? '' : value;
+
+    	let query = new URLSearchParams(window.location.search);
+
+    	return value + (query.has('at') ? query.get('at') : '@jmail.ovh');
+    }
+
+    let elMailBox = document.querySelector('#mailBox');
+    let elFullMailBox = document.createElement('INPUT');
+    let elFullMailBoxFormGroup = document.createElement('DIV');
+    let elFullMailBoxInputGroup = document.createElement('DIV');
+    let elCopyMailWrapper = document.createElement('SPAN');
+    let elCopyMailBtn = document.createElement('BUTTON');
+   
+    elCopyMailWrapper.className = 'input-group-btn';
+    elCopyMailBtn.className = 'btn btn-success';
+    elCopyMailBtn.innerText = 'Copy';
+
+    elCopyMailBtn.addEventListener('click', function() {
+    	elFullMailBox.focus();
+    	elFullMailBox.select();
+    	document.execCommand('copy');
+    	elFullMailBox.setSelectionRange(0,0);
+    	elFullMailBox.blur();
+
+		elCopyMailBtn.innerText = 'Copied';
+
+    	setTimeout(function() {
+    		elCopyMailBtn.innerText = 'Copy';
+    	}, 1000);
+    })
+
+    elCopyMailWrapper.append(elCopyMailBtn);
+
+    elFullMailBox.className = 'form-control';
+    elFullMailBox.setAttribute('readonly', 'readonly');
+
+    elFullMailBoxInputGroup.className = 'input-group';
+    elFullMailBoxFormGroup.className = 'form-group';
+
+    elFullMailBoxInputGroup.append(elFullMailBox);
+    elFullMailBoxInputGroup.append(elCopyMailWrapper);
+
+    elFullMailBoxFormGroup.append(elFullMailBoxInputGroup);
+
+    document.querySelector('.panel-body').children[0].prepend(elFullMailBoxFormGroup);
+
+    elMailBox.addEventListener('input', function(ev) {
+        elFullMailBox.value = fmtMail(elMailBox.value);
+    });
+
+    document.querySelector('#mainForm a[href^="javascript:goFastMailBox"]').addEventListener('click', function(ev) {
+      	elFullMailBox.value = fmtMail(ev.currentTarget.getAttribute('href').replace(/^javascript:goFastMailBox\('/, '').replace(/'\)$/, ''));
+    });
+
+    if (elMailBox.value) {
+    	elFullMailBox.value = fmtMail(elMailBox.value);
+    }
+})();
